3bec1cdd9f85e06d18912b3155dce162d06a0c04
ani <anitha.s.goutham@gmail.com>
2024-10-02 11:49:00 +0800

    roles

--- output.tf
+++ output.tf
@@ -7,11 +7,11 @@ output "public_subnet_ids" {
 }
 
 output "private_subnet_ids" {
-    value = aws_subnet.public[*].id
+    value = aws_subnet.private[*].id
 }
 
 output "database_subnet_ids" {
-    value = aws_subnet.public[*].id
+    value = aws_subnet.database[*].id
 }
 
 # output "az_info" {
